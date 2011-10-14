e615a2bcd504c745f3b465e4ae7a0f2d56dd9a3b
Rob Decker <rob@deckerdev.com>
2011-10-13 18:42:47 -0600

    ongoing youth channel

--- sites/default/themes/mnn/templates/node-yc_join.tpl.php
+++ sites/default/themes/mnn/templates/node-yc_join.tpl.php
@@ -1,11 +1,5 @@
 <article class="yc-join node <?php print $classes; ?>" id="node-<?php print $node->nid; ?>">
-  <header>
-    <?php if (!$page): ?>
-      <h2 class="title"><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
-    <?php endif; ?>
 
-  </header>
-  
     <div class="content">
 	    <?php
 		    if ($node->field_image[0]['filepath'])
--- sites/default/themes/mnn/templates/node-yc_service.tpl.php
+++ sites/default/themes/mnn/templates/node-yc_service.tpl.php
@@ -1,13 +1,9 @@
 <article class="yc-service node <?php print $classes; ?>" id="node-<?php print $node->nid; ?>">
-  <header>
-    <?php if (!$page): ?>
-      <h2 class="title"><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
-    <?php endif; ?>
 
-  </header>
-  
     <div class="content">
 	    <?php
+		    if ($node->field_image[0]['filepath'])
+		      print theme('imagecache', 'yc-our-services', $node->field_image[0]['filepath']);
         print $node->content['body']['#value'];
       ?>
     </div>
