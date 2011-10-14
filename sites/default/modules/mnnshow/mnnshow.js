68a0ff84e40ff11f53c331cdf9c062264575eb64
Rob Decker <rob@deckerdev.com>
2011-10-14 10:09:13 -0600

    initial yc-schedule

--- sites/default/modules/mnnshow/mnnshow.js
+++ sites/default/modules/mnnshow/mnnshow.js
@@ -139,6 +139,149 @@ var schedule = (function(){
 	}
 })();
 
+
+var ycSchedule = (function(){
+
+	var schedDate = '';
+	var prevDate = '';
+	var nextDate = '';
+	var offset, table, headerHeight;
+
+	function init(){
+		generateHtml();
+		offset = $('#schedule-header').offset().top;
+		loadTableHeader()
+		//loadSchedule('2011-05-15');
+		loadSchedule(schedDate);
+	}
+
+	function generateHtml(){
+		var nav = '<a href="#" class="prev"><span>Previous</span></a><a href="#" class="next"><span>Next</span></a>';
+		nav += '<div class="schedule-date"><span class="time"></span><input type="text" id="date-picker" name="date-picker"></div> ';
+		$('#schedule-header .nav').html(nav);
+	}
+
+	function setupEvents(){
+		$('#schedule-header .nav a.prev').click(function(){
+			loadSchedule(prevDate);
+			return false;
+		});
+		$('#schedule-header .nav a.next').click(function(){
+			loadSchedule(nextDate);
+			return false;
+		});
+
+		$(window).scroll(function(){
+		/*	var cols = $('#cols').offset().top + $('#cols').outerHeight();
+			var header = $('#schedule-header .inner').offset().top + $('#schedule-header .inner').outerHeight();
+			console.log('cols: '+cols);
+			console.log('header: '+header);
+			if (cols < header) {
+				$('#schedule-header .inner').css('position', 'relative').removeClass('floating');
+			}
+			else */
+			if ($(window).scrollTop() > (table - headerHeight)){
+				var top = $('#schedule').height() - headerHeight;
+				$('#schedule-header .inner').css('position', 'absolute').addClass('floating').css('top', top);
+			}
+			else if ($(window).scrollTop() >= offset){
+				$('#schedule-header .inner').css('position', 'fixed').addClass('floating').css('top', 0);
+			}
+			else {
+				$('#schedule-header .inner').css('position', 'relative').removeClass('floating').css('top', 0);
+			}
+		});
+		$('#date-picker').datepicker({
+			onClose: function(dateText, inst){
+				if (dateText != schedDate){
+					loadSchedule(dateText);
+				}
+			},
+			showOn: 'button',
+			buttonImageOnly: true,
+			buttonImage: '/sites/default/themes/mnn/images/icon_calendar.png'
+		});
+	}
+
+	function loadTableHeader(){
+		$.ajax({
+			type: 'POST',
+			url: '/schedule/header',
+			dataType: 'json',
+			success: function(data){
+				var cols = '';
+
+				var col = '<div class="col col-time">';
+				col += '<div class="cell heading time"></div>';
+				col += '</div>';
+				cols += col;
+
+				for (var i = 1; i <= 4; i++ ){
+					col = '<div class="col col-' + i + '">';
+					col += '<div class="cell heading ch'+i+'">'+data['channel'+i].name+'</div>';
+					col += '</div>';
+					cols += col;
+				}
+
+				$('#schedule-header .channels').html(cols);
+				$('#schedule-header').height($('#schedule-header .nav').outerHeight() + $('#schedule-header .channels').outerHeight()); // fix the height
+				headerHeight = $('#schedule-header').outerHeight();
+			}
+		});
+	}
+
+	function loadSchedule(date){
+		$.ajax({
+			type: 'POST',
+			url: '/schedule/get',
+			dataType: 'json',
+			data: 'date=' + date,
+			success: function(data){
+				schedDate = data.schedDate;
+				prevDate = data.prevDate;
+				nextDate = data.nextDate;
+				$('#schedule-header .schedule-date .time').html(data.displayDate);
+
+				var cols = '';
+
+				var colTime = '<div class="col col-time">';
+				var current, primetime;
+				for (var i = 0; i < 48; i++){
+					data.time[i].isCurrent == 'true' ? current = ' current' : current = '';
+					data.time[i].isPrimetime == 'true' ? primetime = ' primetime' : primetime = '';
+					colTime += '<div class="cell cell-'+i+' t'+data.time[i].duration+' s'+data.time[i].start+current+primetime+'"><div class="cell-inner">'+ data.time[i].display + '</div></div>';
+				}
+				colTime += '</div>';
+				cols += colTime;
+
+				for (var i = 1; i <= 4; i++ ){
+					var col = '<div class="col col-' + i + '">';
+					var channel = data['ch' + i];
+					for (var j = 0; j < channel.length; j++){
+						var content = channel[j].title;
+						var link = '<a href="'+channel[j].link+'">'+content+'</a>';
+						var category = '<div class="category">'+channel[j].category+'</div>';
+						channel[j].isCurrent == 'true' ? current = ' current' : current = '';
+						channel[j].isPrimetime == 'true' ? primetime = ' primetime' : primetime = '';
+						var cls = 'cell cell-' + j + ' t' + channel[j].duration + ' s' + channel[j].start + current + primetime;
+						col += '<div class="'+ cls +'"><div class="cell-inner">'+link+category+'</div></div>';
+					}
+					col += '</div>';
+					cols += col;
+				}
+				$('#schedule #cols').html(cols);
+				table = $('#schedule').offset().top + $('#schedule').outerHeight();
+				setupEvents();
+			}
+		});
+	}
+
+	return {
+		init: init
+	}
+})();
+
+
 var geolocator = (function() {
 
 	var map, community_boards, masks;
@@ -300,6 +443,9 @@ var geolocator = (function() {
 $(document).ready(function(){
 	if ($('#schedule').length){
 		schedule.init();
-		geolocator.init();
+		//geolocator.init();
+	}
+	if ($('#yc-schedule').length){
+		ycSchedule.init();
 	}
 });
