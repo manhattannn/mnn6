eaecc4232bc57f7780f9fca042a558489a665ea2
Rob Decker <rob@deckerdev.com>
2011-10-21 17:49:19 -0600

    youth channel calendar

--- sites/default/modules/mnnshow/mnnshow.js
+++ sites/default/modules/mnnshow/mnnshow.js
@@ -146,6 +146,7 @@ var ycSchedule = (function(){
 	var prevDate = '';
 	var nextDate = '';
 	var offset, table, headerHeight;
+	var isEventsSetup = false;
 
 	function init(){
 		generateHtml();
@@ -191,6 +192,7 @@ var ycSchedule = (function(){
 				$('#schedule-header .inner').css('position', 'relative').removeClass('floating').css('top', 0);
 			}
 		});
+		
 		$('#date-picker').datepicker({
 			onClose: function(dateText, inst){
 				if (dateText != schedDate){
@@ -201,6 +203,8 @@ var ycSchedule = (function(){
 			buttonImageOnly: true,
 			buttonImage: '/sites/default/themes/mnn/images/icon_calendar.png'
 		});
+
+		isEventsSetup = true;
 	}
 
 	function loadTableHeader(){
@@ -233,7 +237,7 @@ var ycSchedule = (function(){
 	function loadSchedule(date){
 		$.ajax({
 			type: 'POST',
-			url: '/schedule/get',
+			url: '/youth-channel/schedule/get',
 			dataType: 'json',
 			data: 'date=' + date,
 			success: function(data){
@@ -242,36 +246,69 @@ var ycSchedule = (function(){
 				nextDate = data.nextDate;
 				$('#schedule-header .schedule-date .time').html(data.displayDate);
 
-				var cols = '';
+				var wdRows = '';
+				var weRows = '';
 
-				var colTime = '<div class="col col-time">';
-				var current, primetime;
-				for (var i = 0; i < 48; i++){
-					data.time[i].isCurrent == 'true' ? current = ' current' : current = '';
-					data.time[i].isPrimetime == 'true' ? primetime = ' primetime' : primetime = '';
-					colTime += '<div class="cell cell-'+i+' t'+data.time[i].duration+' s'+data.time[i].start+current+primetime+'"><div class="cell-inner">'+ data.time[i].display + '</div></div>';
+				// weekday time row
+				var wdRowTime = '<div class="row row-time row-weekday"><div class="cell day empty"><div class="inner" /></div>';
+				for (var i = 0; i < data.timeRow.weekday.length; i++){
+					wdRowTime += '<div class="cell cell-'+i+' t60"><div class="inner">'+ data.timeRow.weekday[i] + '</div></div>';
 				}
-				colTime += '</div>';
-				cols += colTime;
+				wdRowTime += '</div>';
+				wdRows += wdRowTime;
 
-				for (var i = 1; i <= 4; i++ ){
-					var col = '<div class="col col-' + i + '">';
-					var channel = data['ch' + i];
-					for (var j = 0; j < channel.length; j++){
-						var content = channel[j].title;
-						var link = '<a href="'+channel[j].link+'">'+content+'</a>';
-						var category = '<div class="category">'+channel[j].category+'</div>';
-						channel[j].isCurrent == 'true' ? current = ' current' : current = '';
-						channel[j].isPrimetime == 'true' ? primetime = ' primetime' : primetime = '';
-						var cls = 'cell cell-' + j + ' t' + channel[j].duration + ' s' + channel[j].start + current + primetime;
-						col += '<div class="'+ cls +'"><div class="cell-inner">'+link+category+'</div></div>';
+				// weekend time row
+				var weRowTime = '<div class="row row-time row-weekend"><div class="cell day empty"><div class="inner" /></div>';
+				for (var i = 0; i < data.timeRow.weekend.length; i++){
+					weRowTime += '<div class="cell cell-'+i+' t60"><div class="inner">'+ data.timeRow.weekend[i] + '</div></div>';
+				}
+				weRowTime += '</div>';
+				weRows += weRowTime;
+
+				var current;
+				// weekday show rows
+				for (var i = 1; i <= 5; i++){
+					var wdShowRow = '<div class="row row-shows row-weekday">';
+					wdShowRow += '<div class="cell day"><div class="inner">'+data.dayCol.weekday[i]+'</div></div>';
+					if (Object.keys(data.shows.weekday).length > 0)
+						var numShows = data.shows.weekday[i].length;
+					for (var j = 0; j < numShows; j++) {
+						var day = data.shows.weekday[i];
+						var content = day[j].title;
+						var link = '<div class="link-wrapper"><a href="'+day[j].link+'">'+content+'</a></div>';
+						var category = '<div class="category">'+day[j].category+'</div>';
+						day[j].is_today == 'true' ? current = ' current' : current = '';
+						var cls = 'cell cell-' + j + ' t' + day[j].duration + ' s' + day[j].start + current;
+						wdShowRow += '<div class="'+ cls +'"><div class="inner">'+link+category+'</div></div>';
 					}
-					col += '</div>';
-					cols += col;
+					wdShowRow += '</div> '
+					wdRows += wdShowRow
 				}
-				$('#schedule #cols').html(cols);
-				table = $('#schedule').offset().top + $('#schedule').outerHeight();
-				setupEvents();
+
+				// weekend show rows
+				for (var i = 6; i <= 7; i++){
+					var weShowRow = '<div class="row row-shows row-weekend">';
+					weShowRow += '<div class="cell day"><div class="inner">'+data.dayCol.weekend[i]+'</div></div>';
+					if (Object.keys(data.shows.weekend).length > 0)
+						var numShows = data.shows.weekend[i].length;
+					for (var j = 0; j < numShows; j++) {
+						var day = data.shows.weekend[i];
+						var content = day[j].title;
+						var link = '<div class="link-wrapper"><a href="'+day[j].link+'">'+content+'</a></div>';
+						var category = '<div class="category">'+day[j].category+'</div>';
+						day[j].is_today == 'true' ? current = ' current' : current = '';
+						var cls = 'cell cell-' + j + ' t' + day[j].duration + ' s' + day[j].start + current;
+						weShowRow += '<div class="'+ cls +'"><div class="inner">'+link+category+'</div></div>';
+					}
+					weShowRow += '</div> '
+					weRows += weShowRow
+				}
+
+				$('#yc-schedule #weekday .rows').html(wdRows);
+				$('#yc-schedule #weekend .rows').html(weRows);
+				table = $('#yc-schedule').offset().top + $('#yc-schedule').outerHeight();
+				if (!isEventsSetup)
+					setupEvents();
 			}
 		});
 	}
